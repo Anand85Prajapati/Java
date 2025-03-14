1bd5ad0ecaffb42217774a39d77699e69dbfbb1c
Anand Prajapati <anandprajapati8508@gmail.com>
2025-03-15 04:18:30 +0530

    Add files via upload

--- javaBasics.java
+++ javaBasics.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 import java.util.*;
 public class javaBasics {
     public static void main(String args[]) { 
-     Scanner sc = new Scanner(System.in);
-     int number = sc.nextInt();
-     if(number % 2 == 0){
-      System.out.println("evev");
-     }
-     else{
-      System.out.println("odd");
-     }
+        int age = 10;
+        if(age >= 18){
+          System.out.println("adult");
+        } else 
+        if(age >= 13 && age <18){
+          System.out.println("teeneager");
+        } else {
+          System.out.println("child");
+    }
   }
 }
